e424862d151db6d86c763f566ad2c4a0757e46bd
Matvey Tataurov <71022747+KcasTischaWattt@users.noreply.github.com>
2023-06-21 23:18:54 +0300

    Delete order-managment/order-managment directory

--- order-managment/order-managment/src/main/java/com/vkusnoitochka/ordermanagment/reposiroty/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vkusnoitochka.ordermanagment.reposiroty;
-
-import com.vkusnoitochka.ordermanagment.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
